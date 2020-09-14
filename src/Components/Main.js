dd68f5d3a8e275ae04b22f213a1b3b7d2ee7a082
justsomeguy <thenaders3@gmail.com>
2020-09-13 20:31:37 -0700

    inital commit

---
+++ src/Components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div className='main'>
+      <div className='upperNav'>
+        dummyText
+      </div>
+      <div className='mainContent'>
+        <h1>Here</h1>
+      </div>
+    </div>
+  )
+}
---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className='navBar'>
+      <div className='logo'>
+        logo
+      </div>
+      <ul>
+        <li className='active'>ERC20</li>
+        <li className='active'>ERC721</li>
+        <li className='active'>Dapps</li>
+      </ul>
+    </div>
+  )
+}
